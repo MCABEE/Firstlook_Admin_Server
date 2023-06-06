65eaa9e94e57f391331da09df9bd1211f8f0f6a1
Mohammed fayis <90344973+mohmd-fayis@users.noreply.github.com>
2023-06-06 11:32:13 +0530

    environment changed to production

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import cors from 'cors'
 import logger from 'morgan'
 import AppError from './util/appError'
 import dataManager from './routes/dataManager'
-import auth from './routes/authentication'
+// import auth from './routes/authentication'
 import errorHandler from './middleware/errorHandler'
 import { authMiddleware } from './middleware/authMiddleware'
 
@@ -16,7 +16,7 @@ app.use(cors({ origin: '*', methods: ["GET", "POST", "DELETE", "PATCH"] }))
 // fixing "413 Request Entity Too Large" errors
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ limit: "5mb", extended: true, parameterLimit: 50000 }))
-app.use(logger('dev'))
+// app.use(logger('dev'))
 
 // Health check
 app.get('/', (req, res) => res.status(200).json({ message: 'OK' }))
